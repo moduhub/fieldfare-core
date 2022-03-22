60db517d9637fda4d528ed9705712cde05d58bd4
Adan Kvitschal <adan@moduhub.com>
2022-03-22 19:46:23 -0300

    Working on versioned set update msg generation and sync

--- src/structures/VersionedSet.js
+++ src/structures/VersionedSet.js
@@ -4,19 +4,15 @@
  * and open the template in the editor.
  */
 
+const HashLinkedTree = require('./HashLinkedTree.js');
 
-module.exports = class VersionedSet {
-	
-	contructor(currentVersion) {
-		
-		this.currentVersion = currentVersion;
-		
-		this.admins = new Map();
-		this.elements = new Set();
+class UpdateMessage {
+
+	constructor() {
 		
 	}
 	
-	validateUpdateMessage(message) {
+	static validate(message) {
 		
 		if('signature' in message === false
 		|| 'source' in message === false
@@ -40,63 +36,155 @@ module.exports = class VersionedSet {
 		}
 	}
 	
-	async update(message) {
+	static async fromResource(hash, source) {
 		
-		try {
-			
-			await this.validateUpdateMessage(message);
-			
-			while(iUpdateMessage.data.prev !== this.currentVersion) {
-			
-				if(iUpdateMessage.prev === '') {
-					//Found chain origin without any match
-					throw 'version msg not in chain';
-				}
+		const resourceObject = await host.getResourceObject(hash, source);
+		
+		UpdateMessage.validate(newMessage);
+
+		var newMessage = new UpdateMessage();
+		
+		Object.assign(newMessage, resourceObject);
 				
-				var iUpdateMessage = null;
+		return newMessage;
+	}
+	
+	async buildChain(messageHash, maxDepth) {
+		
+		var iUpdateMessage = this;
+		var depth = 0;
+		var chain = new Array();
+		
+		do {
+
+			await UpdateMessage.validate(iUpdateMessage);
+
+			const prevHash = iUpdateMessage.data.prev;
+			const currentSource = iUpdateMessage.source;
+
+			if(prevHash === '') {
+				//Found chain origin without any match
+				throw 'version msg not in chain';
+			}
+
+			try {
+
+				//Attemp to get last update from same source
+				iUpdateMessage = await UpdateMessage.fromResource(prevHash, currentSource);
 				
-				try {
-					
-					iUpdateMessage = await host.getResource(iUpdateMessage.data.prev,
-										iUpdateMessage.source);
-				} catch(error) {
-					
-					for(let [adminID, admin] of this.admins) {
-						
-						try {
-							//Attemp all other set admins if owner is not available
-							iUpdateMessage = await host.getResource(iUpdateMessage.data.prev,
-												adminID);
-							
-							break;
-							
-						} catch(error) {
-							//Data not available in this admin, try next
-						}
-						
+			} catch(error) {
+
+				//If could not access same source, attmp all other admins
+				for(let [adminID, admin] of this.admins) {
+
+					try {
+						//Attemp all other set admins if owner is not available
+						iUpdateMessage = await UpdateMessage.fromResource(prevHash, adminID);
+
+						break;
+
+					} catch(error) {
+						//Data not available in this admin, try next
 					}
 
 				}
-				
+
+			}
+
+			if(iUpdateMessage == null
+			|| iUpdateMessage == undefined) {
+			
 				//Searched all hosts with no success
-				if(iUpdateMessage == null
-				|| iUpdateMessage == undefined) {
-					throw 'Failed to fetch a update message inside chain';
+				throw 'Failed to fetch a update message inside chain';
+				
+			}
+
+			chain.push(iUpdateMessage);
+			
+			if(++depth>maxDepth) {
+				throw 'build chain failed: max depth reached';
+			}
+
+		} while(iUpdateMessage.data.prev !== this.currentVersion);
+		
+		return chain;
+	}
+	
+};
+
+module.exports = class VersionedSet {
+	
+	constructor(currentVersion) {
+		
+		this.currentVersion = currentVersion;
+		
+		this.admins = new Map();
+		this.elements = new HashLinkedTree(5);
+			
+	}
+
+	async update(message) {
+		
+		await UpdateMessage.validate(message);
+
+		try {
+		
+			var chain = await message.buildChain(this.currentVersion, 50);
+			
+			var newTree = Object.assign(new HashLinkedTree, this.elements);
+			
+			//Perform changes
+			for(let message in chain) {
+				
+				if('add' in message.data) {
+					
+					message.data.add.forEach(element => {
+						newTree.add();
+					});
+				
 				}
 				
-				await this.validateUpdateMessage(iUpdateMessage);
+				this.currentVersion = message.data.version;
 			}
 			
-			//If this point was reached, state chain is valid and is a continuation
-			// of the current local version.
-			this.currentVersion = message.data.version;
+			//check if final version matches message specified version
 			
 		} catch (error) {
 			
-			console.log("Versioned Set update error: " + error);
+			console.error("VersionedSet update failed: " + error);
 			
 		}
-		
+			
 	}
 	
+	//Perform local add
+	async add(newElement) {
+		
+		const prevVersion = this.elements.rootHash;
+		
+		const newElementHash = await host.generateResourceHash(newElement);
+		
+		//Perform local changes
+		this.currentVersion = await this.elements.add(newElement);
+		
+		//Create update message
+		var updateMessage = new UpdateMessage();
+		
+		updateMessage.source = host.id;
+		
+		updateMessage.data = {
+			prev: prevVersion,
+			version: this.currentVersion,
+			add: newElementHash
+		}
+		
+		console.log("Update: " + JSON.stringify(updateMessage, null, 2));
+		
+		await host.signMessage(updateMessage);
+		
+		this.latestUpdate = await host.storeResourceObject(updateMessage);
+		
+		console.log("Update: " + JSON.stringify(this.latestUpdate, null, 2)
+			+ "->" + this.latestUpdate);
+	}
 };
