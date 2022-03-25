522085870766afb4eca1155d515429fdffdfcf16
Adan Kvitschal <adan@moduhub.com>
2022-03-25 17:47:48 -0300

    Changed UpdateMessage references to VersionStatement

--- src/VersionControl/VersionedData.js
+++ src/VersionControl/VersionedData.js
@@ -6,111 +6,8 @@
 
 const HashLinkedTree = require('./HashLinkedTree.js');
 
-class UpdateMessage {
+const VersionStatement = require('./VersionStatement.js');
 
-	constructor() {
-		
-	}
-	
-	static validate(message) {
-		
-		if('signature' in message === false
-		|| 'source' in message === false
-		|| 'data' in message === false) {
-			throw 'malformed update message';
-		} else
-		if('prev' in message.data === false
-		|| 'version' in message.data === false) {
-			throw 'malformed update message data';
-		}
-		
-		var source = this.admins.get(message.source);
-		
-		if(source == undefined
-		|| source == null) {
-			throw 'source not in admins list';
-		}
-		
-		if(source.verifyMessage(message) === false) {
-			throw 'message sinature invalid';
-		}
-	}
-	
-	static async fromResource(hash, source) {
-		
-		const resourceObject = await host.getResourceObject(hash, source);
-		
-		UpdateMessage.validate(newMessage);
-
-		var newMessage = new UpdateMessage();
-		
-		Object.assign(newMessage, resourceObject);
-				
-		return newMessage;
-	}
-	
-	async buildChain(messageHash, maxDepth) {
-		
-		var iUpdateMessage = this;
-		var depth = 0;
-		var chain = new Array();
-		
-		do {
-
-			await UpdateMessage.validate(iUpdateMessage);
-
-			const prevHash = iUpdateMessage.data.prev;
-			const currentSource = iUpdateMessage.source;
-
-			if(prevHash === '') {
-				//Found chain origin without any match
-				throw 'version msg not in chain';
-			}
-
-			try {
-
-				//Attemp to get last update from same source
-				iUpdateMessage = await UpdateMessage.fromResource(prevHash, currentSource);
-				
-			} catch(error) {
-
-				//If could not access same source, attmp all other admins
-				for(let [adminID, admin] of this.admins) {
-
-					try {
-						//Attemp all other set admins if owner is not available
-						iUpdateMessage = await UpdateMessage.fromResource(prevHash, adminID);
-
-						break;
-
-					} catch(error) {
-						//Data not available in this admin, try next
-					}
-
-				}
-
-			}
-
-			if(iUpdateMessage == null
-			|| iUpdateMessage == undefined) {
-			
-				//Searched all hosts with no success
-				throw 'Failed to fetch a update message inside chain';
-				
-			}
-
-			chain.push(iUpdateMessage);
-			
-			if(++depth>maxDepth) {
-				throw 'build chain failed: max depth reached';
-			}
-
-		} while(iUpdateMessage.data.prev !== this.currentVersion);
-		
-		return chain;
-	}
-	
-};
 
 module.exports = class VersionedData {
 	
@@ -127,7 +24,7 @@ module.exports = class VersionedData {
 
 	async update(pMessage) {
 		
-		await UpdateMessage.validate(pMessage);
+		await VersionStatement.validate(pMessage);
 
 		try {
 		
@@ -136,7 +33,7 @@ module.exports = class VersionedData {
 			//Perform changes sequentially
 			for await (const iMessage of chain) {
 			
-				await UpdateMessage.validate(iMessage);
+				await VersionStatement.validate(iMessage);
 			
 				if(await this.apply(iMessage.data.change, iMessage.data.version) == false) {
 					throw 'Update rejected due to invalid chain';
