5a964d540f85ae1c08d92bf3650e0cff228bf54f
Adan Kvitschal <adan@moduhub.com>
2022-03-28 20:42:13 -0300

    Fixed UDP channel creation and management

--- src/UDPTransceiver.js
+++ src/UDPTransceiver.js
@@ -14,8 +14,10 @@ module.exports = class UDPTransceiver extends Transceiver {
 	constructor(port) {
 		super();
 		
-		this.socket = dgram.createSocket('udp4');
+		this.channelMap = new Map();
 		
+		this.socket = dgram.createSocket('udp4');
+				
 		this.socket.on('error', (err) => {
 			console.log(`server error:\n${err.stack}`);
 			this.socket.close();
@@ -23,19 +25,56 @@ module.exports = class UDPTransceiver extends Transceiver {
 
 		this.socket.on('message', (msg, rinfo) => {
   
-			console.log('Message from ' + rinfo.address + ':' + rinfo.port);
+  			console.log('Message from ' + rinfo.address + ':' + rinfo.port);
 			console.log('Lenght: ' + msg.length + ' bytes');
   
-			if(this.messageReceivedCallback) {
+			const channelID = rinfo.address + ":" + rinfo.port;
+  
+			console.log("ChannelID: " + channelID);
+  
+			var assignedChannel;
+  
+			//Check if channel is already registered
+			if(this.channelMap.has(channelID)) {
 				
-				var channel = {	type : 'udp',
-						send : (message) => { this.send(message, channel)},
-						info : rinfo};
+				assignedChannel = this.channelMap.get(channelID);
+
+			} else {
 				
-				//Translate binary to JSON
-				// ...
+				//new channel
+				if(this.onNewChannel) {
+
+					assignedChannel = {
+						type : 'udp',
+						send : (message) => { this.send(message, assignedChannel)},
+						info : {
+							address: rinfo.address,
+							port: rinfo.port
+						}
+					};
+
+					this.channelMap.set(channelID, assignedChannel);
+					
+					this.onNewChannel(assignedChannel);
 				
-				this.messageReceivedCallback(msg, channel);
+				} else {
+					
+					console.error("UDPtrx: onNewChannelCallbck not defined");
+					
+				}
+			}
+    
+			if(assignedChannel
+			&& assignedChannel.onMessageReceived) {
+				
+				var messageObject = JSON.parse(msg);
+				
+				assignedChannel.onMessageReceived(messageObject);
+				
+			} else {
+				
+				console.error("UDPtrx: no messageReceivedCallback defined");
+				
 			}
   
 		});
@@ -61,22 +100,18 @@ module.exports = class UDPTransceiver extends Transceiver {
 		
 		return rNewChannel;
 	}
-	
-	onMessageReceived(callback) {
-		this.messageReceivedCallback = callback;
-	}
-	
+		
 	send(message, channel) {
 		
-		console.log("Message object: " + JSON.stringify(message));
+		console.log("Message object: " + JSON.stringify(message, message.jsonReplacer));
 		
-		var messageBuffer = message.toBuffer();
+		var messageBuffer = JSON.stringify(message, message.jsonReplacer);//message.toBuffer();
 		
 		console.log('UDPTrx.send ' + messageBuffer.length + ' bytes to '
 			+ channel.info.address + ':'
 			+ channel.info.port);
 		
-		console.log("Message binary: " + Utils.ab2hex(messageBuffer));
+		//console.log("Message binary: " + Utils.ab2hex(messageBuffer));
 		
 		this.socket.send(messageBuffer,
 				channel.info.port,
