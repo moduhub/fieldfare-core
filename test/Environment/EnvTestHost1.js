71676ecb1d2fc89f19caf71e579e3c45d4518269
Adan Kvitschal <adan@moduhub.com>
2022-04-14 10:25:10 -0300

    Misc format changes

--- test/Environment/EnvTestHost1.js
+++ test/Environment/EnvTestHost1.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -11,22 +11,22 @@ const Environment = require('../../src/Environment.js');
 
 
 async function test() {
-	
+
 	await common.init(common.privateKeys[0], 15000);
-	
+
 	//Attemp to fetch latest version of environment
 	var env = new Environment();
-	
+
 	await env.init('25b1d0c4-6f12-4a27-bd27-da1b297221d1');
-	
-	await env.addAdmin(host.id);			//me
+
+	await env.addAdmin(host.id);				//me
 	await env.addAdmin(common.testHostsIDs[1]);	//other
-	
+
 	await env.addProvider('resource', common.testHostsIDs[0]);
 	await env.addProvider('resource', common.testHostsIDs[1]);
-	
+
 	console.log("Current env version is " + env.version);
-	
+
 }
 
 test();
