a2c95eebc2f541de8475f7e423a81b0ad1c7d5d3
Adan Kvitschal <adan@moduhub.com>
2022-04-29 20:02:20 -0300

    Corrected version chain length count and common version compare

--- src/versioning/VersionChain.js
+++ src/versioning/VersionChain.js
@@ -15,70 +15,52 @@ module.exports = class VersionChain {
         var iVersion = this.head;
         do {
             const iUpdateMessage = await VersionStatement.fromResource(iVersion, this.owner);
-            yield iUpdateMessage;
+            yield [iVersion, iUpdateMessage];
             iVersion = iUpdateMessage.data.prev;
         } while(iVersion != '');
     }
 
     static async findCommonVersion(chainA, chainB) {
+
         var depthA = 0;
-        for await (const statementA of chainA) {
+
+        for await (const [versionA, statementA] of chainA) {
+
             var depthB = 0;
-            for await (const statementB of chainB) {
-                console.log("A("+depthA+"): " + statementA.data.prev);
-                console.log("B("+depthB+"): " + statementB.data.prev);
-                if(statementA.data.prev === statementB.data.prev) {
-                    return statementA.data.prev;
+
+            for await (const [versionB, statementB] of chainB) {
+
+                console.log("A("+depthA+"): " + versionA);
+                console.log("B("+depthB+"): " + versionB);
+
+                if(versionA === ''
+                || versionB === '') {
+                    throw 'chains not coincident';
+                }
+
+                if(versionA === versionB) {
+                    return versionA;
                 }
                 if(++depthB > chainB.maxDepth) throw 'chain B depth exceeded';
+
             }
+
             if(++depthA > chainA.maxDepth) throw 'chain A depth exceeded';
         }
 
-        throw 'verions chains not coincident';
+        throw 'corrupted chain structure';
     }
 
-    async length(version) {
+    async length(base) {
         var count = 0;
-        if(this.version !== version) {
-            for await(const statement of this) {
+        if(this.head !== base) {
+            console.log("this.head: " + this.head + " base: " + base);
+            for await(const [version, statement] of this) {
                 if(++count>this.maxDepth) throw 'max depth exceeded';
-                if(statement.prev === version) break;
+                if(version === base) break;
             }
         }
         return count;
     }
-
-    async getStatementChain(version) {
-
-		var iUpdateMessage = await VersionStatement.fromResource(this.head, this.owner);
-		var depth = 0;
-		var chain = new Array();
-
-		do {
-
-			await VersionStatement.validate(iUpdateMessage);
-
-			const prevHash = iUpdateMessage.data.prev;
-			const currentSource = iUpdateMessage.source;
-
-			if(prevHash === '') {
-				//Found chain origin without any match
-				throw 'version msg not in chain';
-			}
-
-			//Attemp to get last update from same source
-			iUpdateMessage = await VersionStatement.fromResource(prevHash, this.owner);
-
-			if(++depth>maxDepth) {
-				throw 'build chain failed: max depth reached';
-			}
-
-			chain.push(iUpdateMessage);
-
-		} while(iUpdateMessage.data.prev !== version);
-
-		return chain;
-	}
 
 };
