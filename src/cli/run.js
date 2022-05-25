a3cf42faaacfd4ee68257a8dfa1e79ae88ec3db2
Adan Kvitschal <adan@moduhub.com>
2022-05-25 19:31:45 -0300

    Better run CLI argument parsing

--- src/cli/run.js
+++ src/cli/run.js
@@ -1,31 +1,70 @@
-
+import fs from 'fs';
+import arg from 'arg';
 import {initHost, initEnvironment, initWebports} from './cliCommon';
-
 import winston from 'winston';
 import {logger} from '../basic/Log'
+import chalk from 'chalk';
+
+function parseArgumentsIntoOptions(rawArgs) {
+    const args = arg(
+    {
+        '--dashboard': Boolean,
+        '--daemon': Boolean
+    },
+    {
+        argv: rawArgs.slice(2),
+    }
+    );
+
+    var path = '';
+
+    if(rawArgs[2] && rawArgs[2].search('--')) {
+        path = rawArgs[2];
+    }
+
+    return {
+        path:  path,
+        dashboard: args['--dashboard'] || false,
+        daemon: args['--daemon'] || false
+    };
+}
 
 export async function main(args) {
 
-    console.log('args: ' + args);
+    const options = parseArgumentsIntoOptions(args);
 
     await initHost();
     const env = await initEnvironment();
     await initWebports(env);
 
-    logger.add(new winston.transports.Console({
-       format: winston.format.simple(),
-     }));
+    if(options.dashboard === false
+    && options.daemon === false) {
+        logger.add(new winston.transports.Console({
+           format: winston.format.simple(),
+        }));
+    }
 
-    if(args) {
+    if(options.path !== '') {
+
+        const fullpath = process.cwd() + '\\' + options.path;
+
         try {
-            const {setup} = await import(process.cwd() + '\\' + args);
 
-            await setup(env);
+            if(fs.existsSync(fullpath)) {
+
+                const {setup} = await import(fullpath);
+
+                await setup(env);
+
+            } else {
+                throw Error('File not found');
+            }
         } catch (error) {
-            console.log("Failed to setup service module: " + args);
+            console.log(chalk.red("Failed to setup service module at \'" + options.path + '\': ' + error));
+            process.exit(1);
         }
     } else {
-        console.log("No service defined, running environment basics only");
+        logger.log('info', "No service defined, running environment basics only");
     }
 
 }
