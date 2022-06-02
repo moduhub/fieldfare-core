ecd0b78847ca302ae3a5a70196acb664f275f494
ig-66 <igor@moduhub.com>
2022-06-02 16:56:31 -0300

    Initial levelup resources manager commit and creation of new branch

---
+++ src/resources/LevelUpResourcesManager.js
@@ -0,0 +1,11 @@
+import {ResourcesManager} from './ResourcesManager';
+import {logger} from '../basic/Log';
+
+import asyncstorageDown from 'asyncstorage-down';
+import levelup from 'levelup';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+export class LevelUpResourcesManager extends ResourcesManager{
+
+}
