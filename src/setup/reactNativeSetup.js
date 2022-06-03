a4c3684e3ee992382fa6f31b4e17404e82dab5cc
ig-66 <igor@moduhub.com>
2022-06-03 14:37:17 -0300

    Added the functions into react native setup of MHLib

--- src/setup/reactNativeSetup.js
+++ src/setup/reactNativeSetup.js
@@ -1,2 +1,17 @@
+import { HostManager } from '../HostManager';
+import { LevelUpNVData } from '../nvd/LevelUpNVData';
+import { LevelUpResourcesManager } from '..resources/LevelUpResourcesManager';
 
-//todo
+
+export async function setupReactNativeHost(){
+
+  logger.info("System initHost - React Native");
+
+  global.host = new HostManager();
+
+  global.host = new LevelUpNVData();
+
+  host.addResourcesManager(new LevelUpResourcesManager());
+
+  // check webSetup if needed
+}
