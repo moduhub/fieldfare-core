4a9217a3089d71333e320f28660869eb2b9474fb
ig-66 <igor@moduhub.com>
2022-06-03 08:34:41 -0300

    Added Levelup resource manager to export

--- src/exports/node.js
+++ src/exports/node.js
@@ -5,6 +5,7 @@ export * from '../UDPTransceiver';
 export * from '../WebServerTransceiver';
 
 export * from '../resources/LevelResourcesManager';
+export * from '../resources/LevelUpResourcesManager';
 
 export * from '../nvd/LevelNVData';
 export * from '../nvd/NodeNVData';
