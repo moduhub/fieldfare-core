9dadbe1463b5179bf3255985555444e7ebabebd2
Adan Kvitschal <adan@moduhub.com>
2022-06-30 19:12:52 -0300

    Removed debug logs and excess blank lines

--- src/structures/HashLinkedList.js
+++ src/structures/HashLinkedList.js
@@ -107,56 +107,39 @@ export class HashLinkedList {
 	}
 
 	async append(element) {
-
 		if(this.readOny) {
 			throw Error('Attempt to edit a read only hash linked list');
 		}
-
 		const currentNumElements = await this.getNumElements();
-
 		var newListElement = {
 			prev: this.lastHash,
 			index: currentNumElements,
 			objKey: await ResourcesManager.storeResourceObject(element)
 		};
-
 		//logger.debug("[HLL append] newListElement: " + JSON.stringify(newListElement));
-
 		this.lastHash = await ResourcesManager.storeResourceObject(newListElement);
-
 		this.numElements++;
-
 		// logger.log('info', "hash after append: " + this.lastHash);
-
 	}
 
 	async* [Symbol.asyncIterator]() {
-
 		var iNodeKey = this.lastHash;
-
 		while(iNodeKey !== '') {
-
 			const iNode = await ResourcesManager.getResourceObject(iNodeKey, this.ownerID);
-
-			logger.debug('[HLL Iterator] iNode: ' + JSON.stringify(iNode));
-
+			// logger.debug('[HLL Iterator] iNode: ' + JSON.stringify(iNode));
 			if(iNode === null
 			|| iNode === undefined) {
 				throw Error('HashLinkedList: resource is null or undefined');
 			}
-
 			if(iNode.obj) {
 				logger.warn('[HLL Iterator] Iterating deprecated HLL format');
 				yield iNode.obj;
 			} else {
 				const object = await ResourcesManager.getResourceObject(iNode.objKey, this.ownerID);;
-				logger.debug('[HLL iterator] Object: ' + JSON.stringify(object));
+				// logger.debug('[HLL iterator] Object: ' + JSON.stringify(object));
 				yield object;
 			}
-
 			iNodeKey = iNode.prev;
 		}
-
 	}
-
 };
