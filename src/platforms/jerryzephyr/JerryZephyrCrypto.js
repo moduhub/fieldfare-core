eaa0a659583c870a2d6c62c94d8e511ea2443b52
Adan Kvitschal <adan@moduhub.com>
2023-01-23 15:19:50 -0300

    Added skeleton functions for Jerry Crypto

--- src/platforms/jerryzephyr/JerryZephyrCrypto.js
+++ src/platforms/jerryzephyr/JerryZephyrCrypto.js
@@ -1,14 +1,80 @@
 import {CryptoManager} from '../../basic/CryptoManager';
 
 class JerryZephyrCrypto extends CryptoManager {
+    
     constructor() {
         //
     }
 
-    loadPrivateKey() {
-        //return private key in jwk format
+    static init() {
+        CryptoManager.singleton(new JerryZephyrCrypto);
     }
 
-    //adapt js crypto calls to uECC inside ZephyrOS
+    async generateLocalKeypair() {
+        return {
+            publicKey: {
+                platformData: 0 //pointer/index to the key in C code
+            },
+            privateKey: {
+                platformData: 0 //pointer/index to the key in C code
+            }
+        }
+    }
+
+    async getLocalKeypair() {
+        // Get public/private key pointer from C code
+            //Optional, generate key if none exists on first call to this method
+        return {
+            publicKey: {
+                platformData: 0 //pointer/index to the key in C code
+            },
+            privateKey: {
+                platformData: 0 //pointer/index to the key in C code
+            }
+        }
+    }
+
+    importPublicKey(keyData) {
+        //Import public key from JWK data
+        //returns publicKey as in the keypair object example above, containing platformData
+        return {
+            platformData: 0 //index to the key in C code
+        };
+    }
+
+    exportPublicKey(publicKey) {
+        //The code below is an example only
+        return {
+            "kty": "EC",
+            "use": "sig",
+            "crv": "P-256",
+            "kid": "kiC-s4Hx_v6XkyrIUH1ezlYVXoeES-rByL23S6H0-SQ",
+            "x": "s1_Z1YDpyqXZ3vaOr8JSpRq8BN1mfvCAmbEHSn6s1dQ",
+            "y": "Qg7EG8mFC4hCxxGfaW75kUkxdjSEczbP2D8CAk-HrRQ",
+            "alg": "ES256"
+        }
+    }
+
+    async digest(buffer) {
+        // native call: zephyr_digest using SHA256
+        //buffer: uint8 buffer containing data to be digested
+        //returns: uint8 buffer containing SHA256 digest
+        digestBuffer = new Uint8Array(0);
+        return digestBuffer;
+    }
+
+    signBuffer(buffer, privateKey) {
+		// native call: something like zephyr_sign_buffer(buffer, privateKey.platformData)
+        
+        return signatureBuffer;
+    }
+
+    verifyBuffer(dataBuffer, signatureBuffer, publicKey) {
+        // native call: something like zephyr_verify_buffer(dataBuffer, signatureBuffer, publicKey.platformData)
+        //dataBuffer -> data to be signed, uint8_buffer
+        //signatureBuffer -> buffer containign signature, uint8_buffer
+        //publicKey pointer to the public key assigned to that signature
+        return true; //return true if verify pass, false otherwise
+    }
 
 }
--- src/platforms/jerryzephyr/JerryZephyrSetup.js
+++ src/platforms/jerryzephyr/JerryZephyrSetup.js
@@ -1,18 +1,16 @@
-import {LocalHost} from '../../env/LocalHost';
-import {NVD} from '../../basic/NVD';
-import {JerryZephyrCrypto} from './JerryZephyrCrypto';
-import {FileSystemNVD} from '../shared/FileSystemNVD';
-import {FileSystemResourcesManager} from '../shared/FileSystemResourcesManager';
-import {logger} from '../../basic/Log';
+import { LocalHost } from '../../env/LocalHost';
+import { JerryZephyrCrypto } from './JerryZephyrCrypto';
+import { cryptoManager } from '../../basic/CryptoManager';
+import { FileSystemNVD } from '../shared/FileSystemNVD';
+import { FileSystemResourcesManager } from '../shared/FileSystemResourcesManager';
+import { logger } from '../../basic/Log';
 
 export * from '../shared/CommonSetup';
 
 export async function setupLocalHost() {
-    if(global.crypto === undefined) {
-        global.crypto = JerryZephyrCrypto;
-    }
     FileSystemNVD.init();
     FileSystemResourcesManager.init();
-    const privateKeyData = JerryZephyrCrypto.loadPrivateKey();
-    LocalHost.init(privateKeyData);
+    JerryZephyrCrypto.init();
+    const localKeypair = await cryptoManager.getLocalKeypair();
+    LocalHost.init(localKeypair);
 }
