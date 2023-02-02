092c32821111fe8ede91443ca9ef3e11317b6e42
Adan Kvitschal <adan@moduhub.com>
2023-02-02 18:03:45 -0300

    Fixed Chunking utils function declarations Added more identifier validation steps

--- src/chunking/ChunkingUtils.js
+++ src/chunking/ChunkingUtils.js
@@ -6,42 +6,48 @@ const chunkIdentiferPrefix = 'd:';
 
 export const ChunkingUtils = {
     
-    isValidIdentifier(id) {
-        if(id.length === 46) {
-            const prefix = id.slice(0,2);
-            const base64part = id.slice(2,46);
-            if(prefix === chunkIdentiferPrefix
-            && Utils.isBase64(base64part)) {
-                return true;
+    isValidIdentifier: function (id) {
+        if(id
+        && id !== null
+        && id !== undefined) {
+            if(typeof(id) === 'string') {
+                if(id.length === 46) {
+                    const prefix = id.slice(0,2);
+                    const base64part = id.slice(2,46);
+                    if(prefix === chunkIdentiferPrefix
+                    && Utils.isBase64(base64part)) {
+                        return true;
+                    }
+                }
             }
         }
         return false;
     },
 
-    validateIdentifer(id) {
+    validateIdentifier: function (id) {
         if(ChunkingUtils.isValidIdentifier(id) === false) {
             throw Error('Invalid chunk id: ' + JSON.stringify(id));
         }
     },
 
-    convertObjectToData(object) {
+    convertObjectToData: function (object) {
         const utf8ArrayBuffer = Utils.strToUtf8Array(JSON.stringify(object));
 		const base64data = Utils.arrayBufferToBase64(utf8ArrayBuffer);
         return base64data;
     },
 
-    convertDataToObject(base64data) {
+    convertDataToObject: function (base64data) {
         return JSON.parse(atob(base64data));
     },
 
-    async generateIdentifierForData(base64data) {
+    generateIdentifierForData: async function (base64data) {
         const dataBuffer = Utils.base64ToArrayBuffer(base64data);
         const hashBuffer = await cryptoManager.digest(dataBuffer);
         const base64hash = Utils.arrayBufferToBase64(hashBuffer);
-        return 'D:'+base64hash;
+        return (chunkIdentiferPrefix+base64hash);
     },
 
-    async generateIdentifierForObject(object) {
+    generateIdentifierForObject: async function (object) {
 		const base64data = ChunkingUtils.convertObjectToData(object);
 		const id = await ChunkingUtils.generateIdentifierForData(base64data);
 		return id;
