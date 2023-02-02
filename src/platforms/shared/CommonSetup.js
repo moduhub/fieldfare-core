e8e24f634ab259ef83969709baf22b40db0788b7
Adan Kvitschal <adan@moduhub.com>
2023-02-02 18:12:16 -0300

    Added a setup function for basic collection types

--- src/platforms/shared/CommonSetup.js
+++ src/platforms/shared/CommonSetup.js
@@ -1,6 +1,10 @@
 import {LocalHost} from '../../env/LocalHost';
 import {Environment} from '../../env/Environment';
 import {NVD} from '../../basic/NVD';
+import { ChunkList } from './CommonExports';
+import { ChunkSet } from './CommonExports';
+import { ChunkMap } from './CommonExports';
+import { VersionedCollection } from './CommonExports';
 import {logger} from '../../basic/Log';
 
 export async function setEnvironmentUUID(uuid) {
@@ -18,4 +22,10 @@ export async function setupEnvironment() {
         LocalHost.serveWebport(webport);
     }
     return env;
+}
+
+export function setupBasicCollectionTypes() {
+    VersionedCollection.registerType('list', ChunkList);
+    VersionedCollection.registerType('set', ChunkSet);
+    VersionedCollection.registerType('map', ChunkMap);
 }
