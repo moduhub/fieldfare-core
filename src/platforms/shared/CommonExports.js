2065b344fa6e3e19e4111fd6fc83bd6e237cc52f
Adan Kvitschal <adan@moduhub.com>
2023-02-02 18:07:40 -0300

    Fixed common exports to new strucutre names

--- src/platforms/shared/CommonExports.js
+++ src/platforms/shared/CommonExports.js
@@ -13,16 +13,16 @@ export * from '../../trx/Message';
 export * from '../../trx/Request';
 export * from '../../trx/Transceiver';
 
-export * from '../../chunking/Chunk
+export * from '../../chunking/Chunk';
 export * from '../../chunking/ChunkingUtils';
 export * from '../../chunking/VolatileChunkManager';
 
-export * from '../../structures/HashLinkedList';
-export * from '../../structures/HashLinkedSet';
-export * from '../../structures/HashLinkedMap';
+export * from '../../structures/ChunkList';
+export * from '../../structures/ChunkSet';
+export * from '../../structures/ChunkMap';
 
 export * from '../../versioning/VersionChain';
-export * from '../../versioning/VersionedData';
+export * from '../../versioning/VersionedCollection';
 export * from '../../versioning/VersionStatement';
 
 export * from './CommonSetup';
