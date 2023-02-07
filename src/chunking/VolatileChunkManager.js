8bf8fa26f56fc9ae7affc5506d99084dfd910a77
Adan Kvitschal <adan@moduhub.com>
2023-02-07 18:06:28 -0300

    removed blank lines and debug messages

--- src/chunking/VolatileChunkManager.js
+++ src/chunking/VolatileChunkManager.js
@@ -8,15 +8,12 @@ export class VolatileChunkManager extends ChunkManager {
 
     constructor(enableReport=false) {
         super();
-
         this.hashmap = new Map;
-
         if(enableReport) {
             setInterval(() => {
                 logger.log('info', "Volatile Chunk Manager: " + this.hashmap.size + " chunks stored.");
             }, 30000);
         }
-
     }
 
     static init() {
@@ -25,30 +22,18 @@ export class VolatileChunkManager extends ChunkManager {
     }
 
     async storeChunkContents(base64data) {
-
         const id = await ChunkingUtils.generateIdentifierForData(base64data);
-
 		this.hashmap.set(id, base64data);
-
-		// logger.log('info', "res.store("
-		// 	+ id
-		// 	+ ", "
-		// 	+ base64data
-		// 	+ ") >hashmap size: " + this.hashmap.size);
-
 		return id;
 	}
 
 	getChunkContents(id) {
-
 		var base64data = this.hashmap.get(id);
-
         if(base64data === undefined) {
             const error = Error('Chunk not found');
             error.name = 'NOT_FOUND_ERROR';
             throw error;
         }
-
 		return base64data;
 	}
 
