7a9ec77b9b9008bf820179b483ba2af64a9cd1a9
Adan Kvitschal <adan@moduhub.com>
2023-02-09 20:13:34 -0300

    Added some basic ChunkList tests

---
+++ test/structures/list_basic.test.js
@@ -0,0 +1,50 @@
+import {
+    Chunk,
+    VolatileChunkManager,
+    TestCryptoManager,
+    ChunkList,
+    logger
+} from 'fieldfare/test';
+
+const numCreatedChunks = 100;
+
+const createdChunks = [];
+
+const chunkList = new ChunkList(5);
+
+jest.setTimeout(30000);
+
+beforeAll(async () => {
+    logger.disable();
+    TestCryptoManager.init();
+    VolatileChunkManager.init();
+    for (var i=0; i<numCreatedChunks; i++) {
+        const chunk = await Chunk.fromObject({
+            index: i,
+            salt: 'akftw'
+        });
+        createdChunks.push(chunk);
+    }
+    return;
+});
+
+test('ChunkList stores '+numCreatedChunks+' chunks', async () => {
+    await expect(chunkList.getNumElements()).resolves.toBe(0);
+    var currentNumElements=0;
+    for(const chunk of createdChunks) {
+        await chunkList.append(chunk);
+        currentNumElements++;
+        await expect(chunkList.getNumElements()).resolves.toBe(currentNumElements);
+    }
+    return;
+});
+
+test('ChunkList iterates chunks in reverse order', async () => {
+    var index = createdChunks.length-1;
+    for await(const chunk of chunkList) {
+        const expectedChunkIdentifier = createdChunks[index].id;
+        expect(chunk.id).toBe(expectedChunkIdentifier)
+        index--;
+    }
+    return;
+});
