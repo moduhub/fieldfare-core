e4315c1f12cc639458979067615a7d9f87dff6b7
Adan Kvitschal <adan@moduhub.com>
2023-02-09 20:13:00 -0300

    Adjusted Chunk tests to new Chunk expansion rules

--- test/chunking/chunk_basic.test.js
+++ test/chunking/chunk_basic.test.js
@@ -1,9 +1,11 @@
 import {
     Chunk,
+    ChunkingUtils,
     VolatileChunkManager,
     NodeCryptoManager,
     logger
 } from 'fieldfare/node';
+import { Utils } from '../../src/basic/Utils';
 
 var gTestPersonChunk;
 
@@ -40,8 +42,23 @@ beforeAll(async () => {
     return;
 });
 
-test('Chunk expands correctly to level 0', async () => {
+test('Chunk expands correctly to level 0 (keep identifiers)', async () => {
     const expandedChunk = await gTestPersonChunk.expand(0);
+    // console.log('expandedChunk expanded to level0');
+    // console.log(expandedChunk);
+    Utils.validateParameters(expandedChunk, ['name', 'age', 'car', 'employer']);
+    expect(expandedChunk.name).toBe('John Doe');
+    expect(expandedChunk.age).toBe(34);
+    expect(ChunkingUtils.isValidIdentifier(expandedChunk.car)).toBe(true);
+    expect(ChunkingUtils.isValidIdentifier(expandedChunk.employer)).toBe(true);
+    return;
+});
+
+test('Chunk expands correctly to level 1', async () => {
+    const expandedChunk = await gTestPersonChunk.expand(1);
+    // console.log('expandedChunk expanded to level1');
+    // console.log(expandedChunk);
+    Utils.validateParameters(expandedChunk, ['name', 'age', 'car', 'employer']);
     expect(expandedChunk.name).toBe('John Doe');
     expect(expandedChunk.age).toBe(34);
     expect(expandedChunk.car instanceof Chunk).toBe(true);
@@ -49,41 +66,59 @@ test('Chunk expands correctly to level 0', async () => {
     return;
 });
 
-test('Chunk expands correctly to level 1', async () => {
-    const expandedChunk = await gTestPersonChunk.expand(1);
+test('Chunk expands correctly to level 2', async () => {
+    const expandedChunk = await gTestPersonChunk.expand(2);
+    // console.log('expandedChunk expanded to level2');
+    // console.log(expandedChunk);
+    Utils.validateParameters(expandedChunk, ['name', 'age', 'car', 'employer']);
     expect(expandedChunk.name).toBe('John Doe');
     expect(expandedChunk.age).toBe(34);
+    Utils.validateParameters(expandedChunk.car, ['manufacturer', 'model', 'year']);
     expect(expandedChunk.car.manufacturer instanceof Chunk).toBe(true);
     expect(expandedChunk.car.model).toBe('F-250');
     expect(expandedChunk.car.year).toBe(1999);
+    Utils.validateParameters(expandedChunk.employer, ['name', 'location']);
     expect(expandedChunk.employer.name).toBe('Ford');
     expect(expandedChunk.employer.location instanceof Chunk).toBe(true);
     return;
 });
 
-test('Chunk expands correctly to level 2', async () => {
-    const expandedChunk = await gTestPersonChunk.expand(2);
+test('Chunk expands correctly to level 3', async () => {
+    const expandedChunk = await gTestPersonChunk.expand(3);
+    // console.log('expandedChunk expanded to level3');
+    // console.log(expandedChunk);
     expect(expandedChunk.name).toBe('John Doe');
     expect(expandedChunk.age).toBe(34);
+    Utils.validateParameters(expandedChunk.car, ['manufacturer', 'model', 'year']);
+    Utils.validateParameters(expandedChunk.car.manufacturer, ['name', 'location']);
     expect(expandedChunk.car.manufacturer.name).toBe('Ford');
     expect(expandedChunk.car.manufacturer.location instanceof Chunk).toBe(true);
     expect(expandedChunk.car.model).toBe('F-250');
     expect(expandedChunk.car.year).toBe(1999);
+    Utils.validateParameters(expandedChunk.employer, ['name', 'location']);
+    Utils.validateParameters(expandedChunk.employer.location, ['lat', 'lng']);
     expect(expandedChunk.employer.name).toBe('Ford');
     expect(expandedChunk.employer.location.lat).toBe(42.314936);
     expect(expandedChunk.employer.location.lng).toBe(-83.209773);
     return;
 });
 
-test('Chunk expands correctly to level 3', async () => {
-    const expandedChunk = await gTestPersonChunk.expand(3);
+test('Chunk expands correctly to level 4', async () => {
+    const expandedChunk = await gTestPersonChunk.expand(4);
+    // console.log('expandedChunk expanded to level4');
+    // console.log(expandedChunk);
     expect(expandedChunk.name).toBe('John Doe');
     expect(expandedChunk.age).toBe(34);
+    Utils.validateParameters(expandedChunk.car, ['manufacturer', 'model', 'year']);
+    Utils.validateParameters(expandedChunk.car.manufacturer, ['name', 'location']);
+    Utils.validateParameters(expandedChunk.car.manufacturer.location, ['lat', 'lng']);
     expect(expandedChunk.car.manufacturer.name).toBe('Ford');
     expect(expandedChunk.car.manufacturer.location.lat).toBe(42.314936);
     expect(expandedChunk.car.manufacturer.location.lng).toBe(-83.209773);
     expect(expandedChunk.car.model).toBe('F-250');
     expect(expandedChunk.car.year).toBe(1999);
+    Utils.validateParameters(expandedChunk.employer, ['name', 'location']);
+    Utils.validateParameters(expandedChunk.employer.location, ['lat', 'lng']);
     expect(expandedChunk.employer.name).toBe('Ford');
     expect(expandedChunk.employer.location.lat).toBe(42.314936);
     expect(expandedChunk.employer.location.lng).toBe(-83.209773);
@@ -91,10 +126,14 @@ test('Chunk expands correctly to level 3', async () => {
 });
 
 test('Chunk deduplicates', async () => {
-    const level1Chunk = await gTestPersonChunk.expand(1);
     const level2Chunk = await gTestPersonChunk.expand(2);
-    const locationChunk1 = level1Chunk.employer.location;
-    const locationChunk2 = level2Chunk.car.manufacturer.location;
+    const level3Chunk = await gTestPersonChunk.expand(3);
+    const locationChunk1 = level2Chunk.employer.location;
+    const locationChunk2 = level3Chunk.car.manufacturer.location;
+    // console.log('locationChunk1');
+    // console.log(locationChunk1);
+    // console.log('locationChunk2');
+    // console.log(locationChunk2);
     expect(locationChunk1 instanceof Chunk).toBe(true);
     expect(locationChunk2 instanceof Chunk).toBe(true);
     expect(locationChunk1.id === locationChunk2.id).toBe(true);
