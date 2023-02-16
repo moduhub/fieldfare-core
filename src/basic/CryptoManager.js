e2f2c04a2034bd651da559fab6c168fa3f1bb0d4
Adan Kvitschal <adan@moduhub.com>
2023-02-15 22:36:06 -0300

    Added normalization of messages before sign/verify

--- src/basic/CryptoManager.js
+++ src/basic/CryptoManager.js
@@ -1,5 +1,7 @@
 
+import { Message } from '../trx/Message';
 import { Utils } from './Utils';
+import { logger } from './Log';
 
 export var cryptoManager;
 
@@ -16,7 +18,10 @@ export class CryptoManager {
         if(cryptoManager === undefined) {
             throw Error('CryptoManager not initialized');
         }
-        const utf8ArrayBuffer = Utils.strToUtf8Array(JSON.stringify(message.data));
+        const normalizedMessageData = await Message.normalize(message.data);
+        const utf8ArrayBuffer = Utils.strToUtf8Array(JSON.stringify(normalizedMessageData));
+        console.log('Message to be signed:');
+        console.log(normalizedMessageData);
         const signatureBuffer = await cryptoManager.signBuffer(utf8ArrayBuffer, privateKey);
         message.signature = Utils.uint8ArrayToBase64(signatureBuffer);
     }
@@ -25,11 +30,14 @@ export class CryptoManager {
         if(cryptoManager === undefined) {
             throw Error('CryptoManager not initialized');
         }
+        const normalizedMessageData = await Message.normalize(message.data);
         var result = false;
         if('signature' in message) {
             var signatureBuffer = Utils.base64ToUint8Array(message.signature);
-            var dataBuffer = Utils.strToUtf8Array(JSON.stringify(message.data));
-            result = await CryptoManager.verifyBuffer(dataBuffer, signatureBuffer, publicKey);
+            var dataBuffer = Utils.strToUtf8Array(JSON.stringify(normalizedMessageData));
+            console.log('Message to be verified:');
+            console.log(normalizedMessageData);
+            result = await cryptoManager.verifyBuffer(dataBuffer, signatureBuffer, publicKey);
             logger.log('info', "Signature verify result: " + result);
         } else {
             logger.log('info', 'missing signature inside message: ' + JSON.stringify(message));
--- src/trx/Message.js
+++ src/trx/Message.js
@@ -4,11 +4,29 @@
  * and open the template in the editor.
  */
 
+import { Chunk } from "../chunking/Chunk";
+
 export class Message {
 
 	constructor(service, data) {
 		this.service = service;
 		this.data = data;
+	}
+
+	/**
+	 * Substitute chunks, hosts and any other dedicated objects by their identifiers,
+	 * is a way that the message always hashes to the same value independent of
+	 * any chunk expansion.
+	 * @param {message} message 
+	 * @returns a normalized copy of the message
+	 */
+	static async normalize(message) {
+		const normalizedMessage = new Object;
+		Object.assign(normalizedMessage, message);
+		await Chunk.replaceChunks(normalizedMessage, (key, value) => {
+            return value.id;
+        })
+		return normalizedMessage;
 	}
 
 	setDestinationAddress(address) {
