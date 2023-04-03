67790c0886b0bbacb06fa331e7eddbdcb6c85479
Adan Kvitschal <adan@moduhub.com>
2023-04-03 11:33:52 -0300

    Renamed CLI executables and fixed ESM imports

---
+++ bin/ffconf.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+import {main} from '../src/cli/config.js';
+main(process.argv);
---
+++ bin/ffrun.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+import {main} from '../src/cli/run.js';
+main(process.argv);
