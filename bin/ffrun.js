4032291261ce479e80e93290186606da3d585563
Adan Kvitschal <adan@moduhub.com>
2023-06-13 18:23:46 -0300

    Removed CLI from the core

--- bin/ffrun.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-import {main} from '../src/cli/run.js';
-main(process.argv);
--- bin/ffsetup.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-import {main} from '../src/cli/setup.js';
-main(process.argv);
