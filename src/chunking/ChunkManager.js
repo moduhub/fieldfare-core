bfdd4d43b6fcba2aba4b8666c97ffcaaa7bb34db
Adan Kvitschal <adan@moduhub.com>
2023-06-15 17:53:10 -0300

    removed 3 deprecated methods

--- src/chunking/ChunkManager.js
+++ src/chunking/ChunkManager.js
@@ -29,67 +29,21 @@ export class ChunkManager {
         instances.add(instance);
     }
 
-    static async storeObjectAsChunk(object) {
-		const chunkContents = ChunkingUtils.convertObjectToData(object);
-		const id = await ChunkManager.storeChunkContents(chunkContents);
-//		logger.log('info', "------------------------------\n"
-//			+ "Storing: " + base64hash
-//			+ "->" + JSON.stringify(object, null, 2)
-//			+ "\n------------------------------\n");
-		return id;
-	}
-
-	static async getObjectFromIdentifier(id, owner) {
-		const base64data = await ChunkManager.getChunkContents(id, owner);
-		const object = ChunkingUtils.convertDataToObject(base64data);
-		return object;
-	}
-
 	/**
 	 * Store the contents of a chunk, generating its identifier in the process
 	 * @param {string} contents Chunk contents inbase64 format 
 	 * @returns string containing a chunk identifier assigned to the given data
 	 */
 	static async storeChunkContents(contents) {
-		var base64key;
+		let anyResult;
 		if(ChunkManager.available() === false) {
 	          throw Error('No chunk managers defined');
         }
 		//Store in all defined managers
 		for (const instance of instances) {
-			base64key = await instance.storeChunkContents(contents);
+			anyResult = await instance.storeChunkContents(contents);
 		}
-		return base64key;
-	}
-
-	/**
-	 * Fetch chunk data locally if available, or request from remote host
-	 * otherwise. Throws an error if the chunk is not found anywhere.
-	 * - DEPRECATED: use Chunk.fromIdentifier() instead of managing chunk identifiers manually
-	 * @param {string} id chunk id in base64 format plus prefix
-	 * @param {string} ownerID chunk owner ID in base64 format
-	 * @returns string containing chunk data in base64 format
-	 */
-	static async getChunkContents(id, ownerID) {
-		ChunkingUtils.validateIdentifier(id);
-		var data;
-		try {
-			data = await ChunkManager.getLocalChunkContents(id);
-		} catch(error) {
-			if(error.name === 'NOT_FOUND_ERROR') {
-				//logger.log('info', "res.fetch: Not found locally. Owner: " + owner);
-				if(ownerID === null
-				|| ownerID === undefined) {
-					//Owner not know, fail
-					var error = Error('chunk not found locally, owner unknown: ' + id);
-					error.name = 'NOT_FOUND_ERROR';
-					throw error;
-				}
-				data = await ChunkManager.getRemoteChunkContents(id, ownerID);
-			}                
-			throw error;
-		}
-		return data;
+		return anyResult;
 	}
 	
 	/**
